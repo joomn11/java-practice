ca3c02e89254dc45e5e59b335fd32a2ba5d22c0a
ej <joomn11@gmail.com>
2019-07-08 21:11:42 +0900

    Stream Test Project Add
    Stream Test Project Add

---
+++ src/com/test/StreamTest.java
@@ -0,0 +1,39 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamTest {
+	public static void main(String[] args) {
+		String[] strArr = {"test", "stream", "coding"};
+		List<String> strList = Arrays.asList(strArr);
+		
+		Arrays.sort(strArr);
+		Collections.sort(strList);
+		
+		for(String str : strArr){
+			System.out.println(str);
+		}
+		
+		for(String str: strList){
+			System.out.println(str);
+		}
+		
+		String[] strArr2 = {"test", "stream", "coding"};
+		List<String> strList2 = Arrays.asList(strArr2);
+		
+		// Stream Code 
+		Stream<String> listStream = strList2.stream();
+		Stream<String> arrStream = Arrays.stream(strArr2);
+		
+		listStream.sorted().forEach(System.out::println);
+		arrStream.sorted().forEach(System.out::println);
+		
+		ArrayList<String> aa = listStream.sorted().collect(Collectors.toCollection(ArrayList::new));
+	}
+}
